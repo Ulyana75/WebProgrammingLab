6269db5e03342a7a9858b1d2ee28eae1037eecbc
ulyanaab <ulyanaab@yandex-team.ru>
2021-12-05 00:47:17 +0300

    set min width for swiper

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
 
+	// Стрелки для навигации между слайдами
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev'
 	},
 
+	// Включаем цикличность пролистывания
 	loop: true,
 
+	// Точечки для определения страницы
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true
 	},
 
+	// Количество слайдов на экране будет определяться автоматически
 	slidesPerView: 'auto',
 
+	// Расстояние между слайдами
 	spaceBetween: 85
 });
+
+console.log(swiper)
